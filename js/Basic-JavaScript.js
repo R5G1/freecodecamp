96248a37ffbb59f090996d5fa6f5801c84ff1dba
R5G1 <vlad.klimachevski@gmail.com>
2023-04-26 16:39:09 +0300

    feat: Multiply Two Numbers

--- js/Basic-JavaScript.js
+++ js/Basic-JavaScript.js
@@ -13,4 +13,7 @@
 // const sum = 10 + 10;
 
 //Subtract One Number from Another with JavaScript
-const difference = 45 - 33;
+// const difference = 45 - 33;
+
+//Multiply Two Numbers with JavaScript
+const product = 8 * 10;

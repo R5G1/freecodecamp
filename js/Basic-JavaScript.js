bedeaccd746aaaca27e8564cc2581d41b05e9ce0
R5G1 <vlad.klimachevski@gmail.com>
2023-04-26 16:32:34 +0300

    feat: const Keyword

--- js/Basic-JavaScript.js
+++ js/Basic-JavaScript.js
@@ -1,4 +1,10 @@
 //Explore Differences Between the var and let Keywords
-let catName = 'Oliver';
-let catSound = "Meow!";
-console.log(catName, catSound);
+// let catName = 'Oliver';
+// let catSound = "Meow!";
+// console.log(catName, catSound);
+
+//Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 

8df586dcaee4effc004c78b62d676143bff402e3
R5G1 <vlad.klimachevski@gmail.com>
2023-05-01 16:46:42 +0300

    feat: JavaScript Algorithms and Data Structures

--- js/Basic-JavaScript.js
+++ js/Basic-JavaScript.js
@@ -102,11 +102,15 @@
 // // Only change code below this line
 
 //JavaScript Algorithms and Data Structures
-const myArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
-  
-  const myData = myArray[2][1];
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
+//   const myData = myArray[2][1];
+
+//JavaScript Algorithms and Data Structures
+  // Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray =myArray.pop()
